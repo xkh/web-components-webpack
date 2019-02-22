adf05a9ce16bb3665161a014b949afc80cdee1f6
XKH <xkh@XKHdeMacBook-Pro.local>
2019-02-22 17:01:38 +0800

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'hello-xkh.js',
+        filename: 'hello.xkh.js',
         library: 'xkh',
         libraryTarget: 'umd'
     }
